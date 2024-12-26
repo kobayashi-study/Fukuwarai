1321226c39571646852bd11aa62237a5cf835413
kobayashi-study <yamatokobayashi1225@gmail.com>
2024-12-26 22:10:38 +0900

    PartsControllerを追加
    パーツのドラッグ操作を可能にする制御系スクリプト「PartsController」を追加

--- Assets/Script/PartsController.cs
+++ Assets/Script/PartsController.cs
@@ -4,19 +4,19 @@ using UnityEngine;
 using UnityEngine.Audio;
 
 public class PartsController : MonoBehaviour
-{   
+{
     private AudioSource[] audioSources;
     private bool isMove = true;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     private void OnMouseDown()
@@ -29,12 +29,12 @@ public class PartsController : MonoBehaviour
     //↓顔のパーツをドラッグ操作できる
     private void OnMouseDrag()
     {
-        Vector3 mouse3D =  new Vector3(Input.mousePosition.x, Input.mousePosition.y, 1);
-        Vector3 mouse2D = Camera.main.ScreenToWorldPoint(mouse3D);
+        Vector3 mouseScr = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 1);
+        Vector3 mouseWor = Camera.main.ScreenToWorldPoint(mouseScr);
 
-        if(isMove) 
+        if (isMove)
         {
-            this.transform.position = mouse2D;
+            this.transform.position = mouseWor;
         }
     }
 
@@ -46,10 +46,64 @@ public class PartsController : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.tag == "Button") 
+        if (collision.tag == "Button")
         {
-            Debug.Log("接触中");
             isMove = false;
         }
     }
 }
+    /*
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Button")
+        {
+            Debug.Log("接触終了");
+            isMove = true;
+        }
+    }
+    
+}
+
+/*
+public class PartsController : MonoBehaviour
+{
+    private AudioSource[] audioSources;
+    private GameObject button;
+    private bool isMove = true;
+
+    void Start()
+    {
+        button = GameObject.FindGameObjectWithTag("Button");
+    }
+
+    void OnMouseDown()
+    {
+        audioSources = GetComponents<AudioSource>();
+        audioSources[0].Play();
+    }
+
+    void OnMouseDrag()
+    {
+        Vector3 mouseScr = Input.mousePosition;
+        Vector3 mouseWor = Camera.main.ScreenToWorldPoint(new Vector3(mouseScr.x, mouseScr.y, 1));
+
+
+        float distance = Vector2.Distance(mouseWor, button.transform.position);
+        if (distance < 1)
+        {
+            isMove = false;
+        }
+
+        if (isMove)
+        {
+            transform.position = mouseWor;
+        }
+    }
+
+    void OnMouseUp()
+    {
+        audioSources = GetComponents<AudioSource>();
+        audioSources[1].Play();
+    }
+}
+*/
