aaf853f4b3411333defd988ac936bb089866a289
kobayashi-study <yamatokobayashi1225@gmail.com>
2025-01-17 22:44:58 +0900

    当たり判定の修正等
    ①PartsControllerの「パーツとボタンが重ならないようにする処理」を一部修正
    ②TutorialSceneのTextを修正

--- Assets/Script/PartsController.cs
+++ Assets/Script/PartsController.cs
@@ -5,108 +5,61 @@ using UnityEngine.Audio;
 
 public class PartsController : MonoBehaviour
 {
+    [SerializeField] private Vector2 collision;
     private AudioSource[] audioSources;
-    private bool isMove = true;
     private Vector3 distance;
-    // Start is called before the first frame update
+    private Vector3 validPosition;
+    private Vector3 targetPosition;
+
     void Start()
     {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
+        audioSources = GetComponents<AudioSource>();
+        validPosition = this.transform.position;
     }
 
     private void OnMouseDown()
     {
-        audioSources = GetComponents<AudioSource>();
         audioSources[0].Play();
-        Vector3 mouseScr = new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.WorldToScreenPoint(transform.position).z);
+        Vector3 mouseScr = new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.WorldToScreenPoint(this.transform.position).z);
         Vector3 mouseWor = Camera.main.ScreenToWorldPoint(mouseScr);
         distance = this.transform.position - mouseWor;
     }
 
-    //↓顔のパーツをドラッグ操作できる
     private void OnMouseDrag()
     {
-        Vector3 mouseScr = new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.WorldToScreenPoint(transform.position).z);
+        Vector3 mouseScr = new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.WorldToScreenPoint(this.transform.position).z);
         Vector3 mouseWor = Camera.main.ScreenToWorldPoint(mouseScr);
+        targetPosition = mouseWor + distance;
 
-        if (isMove)
-        {
-            this.transform.position = mouseWor + distance;
-        }
+        MoveObject();
     }
 
     private void OnMouseUp()
     {
-        audioSources = GetComponents<AudioSource>();
         audioSources[1].Play();
     }
 
-    /*private void OnTriggerEnter2D(Collider2D collision)
+    private void MoveObject()
     {
-        if (collision.tag == "Button")
+        Collider2D[] overlaps = Physics2D.OverlapBoxAll(targetPosition, collision, transform.rotation.eulerAngles.z);
+        bool dontMove = false;
+
+        foreach (Collider2D overlap in overlaps)
         {
-            isMove = false;
+            if (overlap.tag == "Button")
+            {
+                dontMove = true;
+                break;
+            }
         }
-    }*/
-}
-    /*
-    private void OnTriggerExit2D(Collider2D collision)
-    {
-        if (collision.tag == "Button")
+        if (!dontMove)
         {
-            Debug.Log("接触終了");
-            isMove = true;
+            transform.position = targetPosition;
+            validPosition = transform.position;
         }
-    }
-    
-}
-
-/*
-public class PartsController : MonoBehaviour
-{
-    private AudioSource[] audioSources;
-    private GameObject button;
-    private bool isMove = true;
-
-    void Start()
-    {
-        button = GameObject.FindGameObjectWithTag("Button");
-    }
-
-    void OnMouseDown()
-    {
-        audioSources = GetComponents<AudioSource>();
-        audioSources[0].Play();
-    }
-
-    void OnMouseDrag()
-    {
-        Vector3 mouseScr = Input.mousePosition;
-        Vector3 mouseWor = Camera.main.ScreenToWorldPoint(new Vector3(mouseScr.x, mouseScr.y, 1));
-
-
-        float distance = Vector2.Distance(mouseWor, button.transform.position);
-        if (distance < 1)
+        else
         {
-            isMove = false;
+            transform.position = validPosition;
         }
-
-        if (isMove)
-        {
-            transform.position = mouseWor;
-        }
-    }
-
-    void OnMouseUp()
-    {
-        audioSources = GetComponents<AudioSource>();
-        audioSources[1].Play();
     }
 }
-*/
